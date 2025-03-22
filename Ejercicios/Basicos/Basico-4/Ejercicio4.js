e848e72718a8293406fc3c5bea05616da933975d
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-03-22 17:30:45 -0500

    feat: :sparkles: Ejercicios Basics 3-4

---
+++ Ejercicios/Basicos/Basico-4/Ejercicio4.js
@@ -0,0 +1,11 @@
+let numero1 = Number(prompt("Primer numero: "))
+let numero2 = Number(prompt("Segundo numero: "))
+let numero3 = Number(prompt("Tercer numero: "))
+
+if (isNaN(numero1,numero2,numero3)){
+    console.log("Ingrese un numero.")
+}
+else{
+    let media = (numero1+numero2+numero3)/3
+    console.log(`La media de los numero es de: ${media.toFixed(0)}.`)
+}
