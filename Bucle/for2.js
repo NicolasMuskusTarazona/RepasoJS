cd66d7aee9f740cc52f0b44dc42ccb0e0a9d9dd3
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-03-30 10:45:13 -0500

    feat: :sparkles: Bucles.

---
+++ Bucle/for2.js
@@ -0,0 +1,4 @@
+const pasos = 5
+for (let paso = 0; paso <=pasos; paso++){
+	console.log("Estoy dando el siguiuente paso " + paso)
+}
---
+++ Bucle/switch1.js
@@ -0,0 +1,15 @@
+let tipoFruta = "Manzanas"
+switch (tipoFruta) {
+    case "Naranjas":
+        console.log("Las Naranjas cuestan $5 ")
+        break;
+    case "Manzanas":
+        console.log("Las Manzanas cuestan $10 ")
+        break;
+    case "Fresas":
+        console.log("Las fresas cuestan $15")
+        break
+    default:
+        console.log("Disculpa no tenemos ese tipo de fruta")
+        break;
+}
