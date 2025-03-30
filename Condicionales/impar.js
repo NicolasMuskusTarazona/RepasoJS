fa06d7fc28ec1d1d1426c3fadd4d2f9d00799481
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-03-30 14:36:07 -0500

    feat: :sparkles: Condicionales impar y par.
    condicionales

---
+++ Condicionales/impar.js
@@ -0,0 +1,6 @@
+let Numero = 1
+if (Numero %2 ==0) { console.log(false);
+}
+else{
+    console.log(true)
+}
---
+++ Condicionales/par.js
@@ -0,0 +1,6 @@
+let Numero = 2
+if (Numero %2 ==0) { console.log(true);
+}
+else{
+    console.log(false)
+}
